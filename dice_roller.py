05105b26a5e9450def6f67888850734410552fe0
elaineqian02 <elaineqian@berkeley.edu>
2021-01-12 14:45:02 -0800

    critical fails and successes

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,23 @@
 import random
 
 def main():
-  dice_rolls, dice_sum = 3, 0
+  dice_rolls = 2
+  dice_sum, num_rolls = 0, 0
 
-  for i in range(0,dice_rolls):
+  while True:
     roll = random.randint(1,6)
     dice_sum += roll
-    print(f'You rolled a {roll}')
+    num_rolls += 1
+    
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Critical Success!')
+      break;
+    else:
+      print(f'You rolled a {roll}')
   
-  print(f'You have rolled a total of {dice_sum}')
+  print(f'You have rolled a total of {dice_sum} in {num_rolls} rolls')
 
 if __name__== "__main__":
   main()
